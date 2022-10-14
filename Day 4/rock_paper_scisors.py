9ce7995c76f4e5fd2c0cba6d6ad4e31dc57fb938
RadoslawJDA <96500398+RadoslawJDA@users.noreply.github.com>
2022-10-15 00:05:39 +0200

    Finished day 4
    finished rock paper scissors game and treasure map

---
+++ Day 4/rock_paper_scisors.py
@@ -0,0 +1,29 @@
+import random
+
+rock = "rock"
+paper = "paper"
+scissors = "scissors"
+choices = [rock, paper, scissors]
+
+rand_int = random.randint(0,2)
+user_input = int(input("Choose 0-Rock 1-Paper 2-Scissors\n"))
+
+if user_input > 2 or user_input < 0:
+    print("You typed and invalid number, (0 - 2)")
+else:
+    user_choice = choices[user_input]
+    rand_choice = choices[rand_int]
+
+    print(f"you chose: {user_choice}")
+    print(f"computer chose: {rand_choice}")
+
+    if user_input == 0 and rand_int == 2:
+        print("you win!")
+    elif user_input == 2 and rand_int == 0:
+        print("you lose!")
+    elif user_input > rand_int:
+        print("you win!")
+    elif user_input == rand_int:
+        print("DRAW!")
+    else:
+        print("you lose!")
--- Day 4/treasure_map.py
+++ Day 4/treasure_map.py
@@ -1,15 +1,14 @@
 
-row0 = ["0 ", "1 ", "2 "]
 row1 = ["⬜️", "⬜️", "⬜️"]
 row2 = ["⬜️", "⬜️", "⬜️"]
 row3 = ["⬜️", "⬜️", "⬜️"]
 island = [row1, row2, row3]
-print(f"{row0}\n{row1}\n{row2}\n{row3}")
+print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? (column and row e.g. 11)\n")
 
 horizontal = int(position[0])
 vertical = int(position[1])
 
-island[horizontal][vertical] = "x"
+island[horizontal-1][vertical-1] = "x"
 
-print(f"{row0}\n{row1}\n{row2}\n{row3}")
+print(f"{row1}\n{row2}\n{row3}")
