dc208766d1dfc6874a1ec59f477ef0d974fa36a0
RadoslawJDA <96500398+RadoslawJDA@users.noreply.github.com>
2022-12-07 18:11:51 +0100

    Lower higher game fix

--- higher-lower/main.py
+++ higher-lower/main.py
@@ -22,19 +22,23 @@ def get_choice():
     print(f"{second_choice['description']} {second_choice['name']} from "
           f"{second_choice['country']} with {second_choice['follower_count']} followers")
 
-    return answers, first_followers, second_followers, first_choice_index
+    return answers, first_followers, second_followers, second_choice_index
 
 
 def another_choice(question, answers2):
     first_choice = data[question]
-
     second_choice = random.choice(data)
+
     index = data.index(second_choice)
     while index in answers2:
         second_choice = random.choice(data)
         index = data.index(second_choice)
 
-    answers2.append(index)
+    answers.append(index)
+
+    first_followers = first_choice['follower_count']
+    second_followers = second_choice['follower_count']
+
     print(f"{first_choice['description']} {first_choice['name']} from "
           f"{first_choice['country']} with {first_choice['follower_count']} followers")
     print('OR')
@@ -45,26 +49,30 @@ def another_choice(question, answers2):
 
 
 # indexes of used dictionaries, followers
-answers, first_followers, second_followers, first_choice_index = get_choice()
+answers, first_followers, second_followers, second_choice_index = get_choice()
 
 # main
 bad_answer = 0
+good_answer = 0
 while bad_answer == 0:
 
-    good_answer = 0
     usr_input = input("Higher or lower H/L: ").upper()
 
+    print(first_followers, second_followers)
     if usr_input == 'H' and first_followers < second_followers:
         good_answer += 1
         os.system('cls')
         print("Let's goo")
-        another_choice(first_choice_index, answers)
+        first_followers, second_followers, answers = another_choice(second_choice_index, answers)
 
     elif usr_input == 'L' and first_followers > second_followers:
         good_answer += 1
         os.system('cls')
         print("Let's goo")
-        another_choice(first_choice_index, answers)
+        first_followers, second_followers, answers = another_choice(second_choice_index, answers)
     else:
         bad_answer = 1
-        print(f'You lost score: {good_answer}')
+        print(f'You lost, score: {good_answer}')
+        break
+
+
